8198a14b6ae021ec889d67afedd396d2d512bbf4
Brian Liew <brianlw1872@gmail.com>
2025-07-28 11:19:56 +0800

    edit homescreen ui borders

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,20 @@ function Home() {
   return (
     <div className="game-container">
       <Navigation />
-      
-      <div className="home text-4xl font-bold text-center mt-10">
+
+      <div className="home text-4xl font-bold text-center mt-4">
         <h1 className="text-3xl">Trading Simulation Application</h1>
-        
-        <div className="hero container max-w-screen-lg mx-auto py-10 flex justify-center">
+
+        <div className="hero container max-w-screen-lg mx-auto py-6 flex justify-center">
           <img
             src="/app-logo.jpg"
             alt="App Logo"
-            width="250" 
+            width="250"
             height="250"
           />
         </div>
 
-        <div className="space-y-4">
+        <div className="space-y-3">
           <Link to="/player-screen">
             <button id="home" className="px-4 py-2 rounded border bg-white hover:bg-gray-100">
               Player
